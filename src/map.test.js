731bd4712283bda622e15a06b10a4869c91ddb04
Bhanu Teja Perumandla <perumandlabhanuteja@gmail.com>
2021-08-03 21:29:49 +0530

    edited maps.test.js to add a test case

--- src/map.test.js
+++ src/map.test.js
@@ -12,5 +12,12 @@ describe('Map',()=>{
     it('return array with elements cubed',()=>{
         expect(map([2,3,4],num=>Math.pow(num,3))).toEqual([8,27,64]);
     });
+
+    it('array with object [a={x:10}] and a=>a.x+1 should give a.x+1',()=>{
+        expect(map([a={x:10}],a=>a.x+1)).toEqual([11]);
+    });
+    it('array with object [a={x:10}] and a=>pow((a.x),3) should give 1001',()=>{
+        expect(map([a={x:10}],a=>Math.pow(a.x,3)+1)).toEqual([1001]);
+    });
     
 });
